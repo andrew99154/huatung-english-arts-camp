9e0f14e96a27aec8397f8311d25532a619876026
宇承 張 <andrew99154@gmail.com>
2023-04-18 12:01:47 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() > 50) { //use `this`, not `document`
+//         $('#top-navbar').css({
+//             // 'display': 'flex',
+//             // 'margin-top': '0',
+//             // 'transition': 'margin-top 1s ease'
+//             'position':'fixed'
+//         });
+//     }
+//     else if ($(this).scrollTop() < 50) { //use `this`, not `document`
+//         $('#top-navbar').css({
+//             // 'display': 'none',
+//             // 'margin-top': '-6em',
+//             'transition': 'margin-top 1s ease-in-out',
+//             'position': 'relative'
+//         });
+//     }
+// });
